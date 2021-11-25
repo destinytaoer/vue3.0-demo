d7175d388caaa6a49e9603cc5355c85891ef99ac
destiny <1848765519@qq.com>
2021-11-25 12:36:26 +0800

    feat: Suspense demo

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,21 @@
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue';
 import TelePortDemo from './components/TeleportDemo.vue';
+import SuspenseDemo from './components/SuspenseDemo.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
   <TelePortDemo />
+  <Suspense>
+    <template #default>
+      <SuspenseDemo />
+    </template>
+    <template #fallback>
+      <p>loading...</p>
+    </template>
+  </Suspense>
 </template>
 
 <style>
---
+++ src/components/SuspenseDemo.vue
@@ -0,0 +1,26 @@
+<template>
+  <p>
+    用户编号: {{ id }}<br />
+    用户姓名: {{ name }}<br />
+  </p>
+</template>
+<script lang="ts">
+const query = () => {
+  return new Promise<{ id: number; name: string }>((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ id: 1, name: 'destiny' });
+    }, 2000);
+  });
+};
+
+export default {
+  name: 'SuspenseDemo',
+  async setup() {
+    const data = await query();
+    return {
+      ...data,
+    };
+  },
+};
+</script>
+<style></style>
